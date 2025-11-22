7a3489479aaeaffc2730e0a59625fcc88c383299
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2025-11-22 15:56:42 +0530

    main build definition

--- src/client/ws_client.h
+++ src/client/ws_client.h
@@ -1,1 +1,45 @@
 # Contents for src/client/ws_client.h file 
+#pragma once
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/ssl.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/websocket/ssl.hpp>
+#include <boost/asio/strand.hpp>
+#include <string>
+#include <functional>
+#include <memory>
+
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace websocket = beast::websocket;
+namespace net = boost::asio;
+namespace ssl = boost::asio::ssl;
+using tcp = boost::asio::ip::tcp;
+
+class WebSocketClient : public std::enable_shared_from_this<WebSocketClient> {
+public:
+    using MessageCallback = std::function<void(const std::string&)>;
+
+    WebSocketClient(net::io_context& ioc, ssl::context& ctx);
+    
+    void Connect(const std::string& host, const std::string& port);
+    void Send(const std::string& message, bool is_binary = false);
+    void SetOnMessage(MessageCallback cb);
+    void Close();
+
+private:
+    void OnResolve(beast::error_code ec, tcp::resolver::results_type results);
+    void OnConnect(beast::error_code ec, tcp::resolver::results_type::endpoint_type ep);
+    void OnSslHandshake(beast::error_code ec);
+    void OnHandshake(beast::error_code ec);
+    void OnWrite(beast::error_code ec, std::size_t bytes_transferred);
+    void OnRead(beast::error_code ec, std::size_t bytes_transferred);
+    void OnClose(beast::error_code ec);
+
+    tcp::resolver resolver_;
+    websocket::stream<beast::ssl_stream<beast::tcp_stream>> ws_;
+    beast::flat_buffer buffer_;
+    std::string host_;
+    MessageCallback on_message_;
+};
